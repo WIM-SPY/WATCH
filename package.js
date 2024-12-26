19a854c54aa9c5a7ae39f42bde3526e2b5a8db04
Wandile Mlangeni <wandilemlangeni3@gmail.com>
2024-12-26 20:13:16 +0200

    structure

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "wacth",
+  "version": "1.0.0",
+  "description": "",
+  "main": "watch.html",
+  "scripts": {
+    "start": "parcel watch.html --open",
+    "build": "parcel build watch.html"
+  },
+  "dependencies": {},
+  "devDependencies": {
+    "@babel/core": "7.2.0",
+    "parcel-bundler": "^1.6.1"
+  },
+  "keywords": []
+}
